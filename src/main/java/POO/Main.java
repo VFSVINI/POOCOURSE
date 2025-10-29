68c2b8b155967cf5e5dbea840da9c116ab4ae87c
Vinícius França <vfs2@cin.ufpe.br>
2025-10-29 02:02:02 -0300

    Iniciando Estudo de POO

---
+++ src/main/java/POO/Main.java
@@ -0,0 +1,12 @@
+package POO;
+
+public class Main {
+    public static void main(String[] args) {
+
+       Pessoa p1 = new Pessoa();
+       Pessoa p2 = new Pessoa();
+
+    }
+}
+
+
---
+++ src/main/java/POO/Pessoa.java
@@ -0,0 +1,14 @@
+package POO;
+
+public class Pessoa {
+
+    //Atributos - Características - Variáveis Que Armazenam Dados e Possuem Modificadores de Acesso
+
+    private String nome;
+    private String cpf;
+    private int idade;
+    private String whatsapp;
+
+    //Construtores
+    //Métodos
+}

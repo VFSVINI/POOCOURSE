7f609a69560103e26d55db320e6b7bf01eb004b5
Vinícius França <vfs2@cin.ufpe.br>
2025-10-29 18:54:53 -0300

    Iniciando Estudo de POO

--- src/main/java/POO/Main.java
+++ src/main/java/POO/Main.java
@@ -4,7 +4,8 @@ public class Main {
     public static void main(String[] args) {
 
        Pessoa p1 = new Pessoa();
-       Pessoa p2 = new Pessoa();
+       Pessoa p2 = new Pessoa("Vini","000",23,"00000");
+       Pessoa p3 = new Pessoa("Vini",23,"000","00000");
 
     }
 }
--- src/main/java/POO/Pessoa.java
+++ src/main/java/POO/Pessoa.java
@@ -10,5 +10,36 @@ public class Pessoa {
     private String whatsapp;
 
     //Construtores
+    //Construtores Padrão
+
+    public Pessoa(){
+
+        nome = "";
+        cpf = "";
+        idade = 0;
+        whatsapp = "";
+    }
+
+    //Construtor Recebendo Parâmetros
+
+    public Pessoa(String nome, String cpf, int idade, String whatsapp) {
+
+        this.nome = nome;
+        this.cpf = cpf;
+        this.idade = idade;
+        this.whatsapp = whatsapp;
+    }
+
+    public Pessoa(String nome,int idade, String cpf, String whatsapp) {
+
+        this.nome = nome;
+        this.cpf = cpf;
+        this.idade = idade;
+        this.whatsapp = whatsapp;
+    }
+
+
+
+
     //Métodos
 }
